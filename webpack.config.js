5965737b0efa05192efb07e7b5bed08f5603f50e
Mike Barile <mikebarile13@gmail.com>
2016-11-14 17:25:20 -0800

    Cleaned up packages and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,7 @@
-const path = require('path');
 module.exports = {
-  context: __dirname,
-  entry: './lib/bubbles',
+  entry: "./lib/bubbles.js",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+  	filename: "./lib/bundle.js"
   },
-  resolve: {
-    extensions: ['', '.js']
-  },
-  node: {
-  fs: "empty"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015']
-        }
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
-    ]
-  },
-  devtool: 'source-maps'
+  devtool: 'source-map',
 };
